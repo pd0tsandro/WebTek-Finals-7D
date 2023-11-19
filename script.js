9508c25f70e23ba1166559fd6ed317235ee2d607
pd0tsandro <115862042+pd0tsandro@users.noreply.github.com>
2023-11-19 12:25:40 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form");
+eField = form.querySelector(".username"),
+eInput = eField.querySelector("input"),
+pField = form.querySelector(".password"),
+pInput = pField.querySelector("input");
+
+form.onsubmit = (e)=>{
+  e.preventDefault(); 
+  (eInput.value == "") ? eField.classList.add("shake", "error") : checkUsername();
+  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+
+  setTimeout(()=>{ 
+    eField.classList.remove("shake");
+    pField.classList.remove("shake");
+  }, 500);
+
+  eInput.onkeyup = ()=>{checkUsername();} 
+  pInput.onkeyup = ()=>{checkPass();} 
+
+  function checkUsername(){ 
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
+    if(!eInput.value.match(pattern)){ 
+      eField.classList.add("error");
+      eField.classList.remove("valid");
+      let errorTxt = eField.querySelector(".error-txt");
+      (eInput.value != "") ? errorTxt.innerText = "Enter a valid Username" : errorTxt.innerText = "Username can't be blank";
+    }else{ 
+      eField.classList.remove("error");
+      eField.classList.add("valid");
+    }
+  }
+
+  function checkPass(){ 
+    if(pInput.value == ""){ 
+      pField.classList.add("error");
+      pField.classList.remove("valid");
+    }else{ 
+      pField.classList.remove("error");
+      pField.classList.add("valid");
+    }
+  }
+
+
+  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
+    window.location.href = form.getAttribute("action"); 
+  }
+}
